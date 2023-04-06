4903cdd257e710ad091782edf3af9a9dee4284d7
Mike Pickering <mpicker0@gmail.com>
2023-04-06 11:21:07 +0000

    Move one-time functions to separate file
    Also, erase functions after evaluating them

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -2,36 +2,22 @@ function fish_prompt
   # Cache exit status
   set --global last_status $status
 
-  # Just calculate these once, to save a few cycles when displaying the prompt
-  if not set --query __fish_prompt_remote
-    set --global __fish_prompt_remote ''
-    if set --query SSH_CONNECTION
-      set --global __fish_prompt_remote 📡
-    end
-  end
-  if not set --query __fish_prompt_char
-    switch (id -u)
-      case 0
-        set --global __fish_prompt_char '#'
-      case '*'
-        set --global __fish_prompt_char '>'
-    end
-  end
-
   function _exit_indicator
     if test $last_status -ne 0
       echo (set_color red)'('$last_status')'(set_color normal)
     else
       echo ''
     end
+    functions --erase _exit_indicator
   end
 
-  function get_arrow_color
+  function _get_arrow_color
     if test $last_status -ne 0
       echo 'red'
     else
       echo 'green'
     end
+    functions --erase _get_arrow_color
   end
 
   # Setup colors
@@ -42,30 +28,23 @@ function fish_prompt
   set --local white (set_color white)
   set --local gray (set_color normal)
   set --local brwhite (set_color --bold white)
-  set --local arrow_color (set_color (get_arrow_color))
+  set --local arrow_color (set_color (_get_arrow_color))
+  set --local prompt_char_color (set_color --bold cyan)
 
-  set prompt_char_color (set_color --bold cyan)
-
-  function venv_status
+  function _venv_status
     if set --query VIRTUAL_ENV
       echo -n ' 🐍 '
     end
+    functions --erase _venv_status
   end
 
-  # Configure __fish_git_prompt
-  set --global __fish_git_prompt_showdirtystate true
-  set --global __fish_git_prompt_showuntrackedfiles true
-  set --global __fish_git_prompt_color green
-  set --global __fish_git_prompt_color_flags red
-  set --global ___fish_git_prompt_char_dirtystate '⚡'
-  set --global ___fish_git_prompt_char_untrackedfiles '+'
   set --local exit_indicator (_exit_indicator)
 
   # Line 1
   echo -n $arrow_color'┌'$cyan$USER$__fish_prompt_remote $normal(prompt_pwd)$normal
   __fish_git_prompt
   __fish_svn_prompt
-  venv_status
+  _venv_status
   echo
 
   # Line 2
